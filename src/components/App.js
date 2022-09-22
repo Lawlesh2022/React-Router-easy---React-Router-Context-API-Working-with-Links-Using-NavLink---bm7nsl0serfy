ae8e20b3f19f2765d529fde5e7232bc562fdc9c0
Lawlesh2022 <106768533+Lawlesh2022@users.noreply.github.com>
2022-09-22 18:10:47 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import { AppRoutes } from '../AppRoutes';
+import {NavBar} from './NavBar'
+import { BrowserRouter } from 'react-router-dom';
 const App = () => {
 
   return (
-    <>
-      <div id="main">        
+    <BrowserRouter>
+      <div id="main"> 
+        <NavBar/>       
         <AppRoutes />
       </div>
-    </>
+    </BrowserRouter>
 
   )
 }
