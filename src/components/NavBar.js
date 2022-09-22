d7f41d5fa364c7d2dc19a64b66d1343f346171b3
Lawlesh2022 <106768533+Lawlesh2022@users.noreply.github.com>
2022-09-22 09:37:29 +0530

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,6 +3,20 @@ import React from 'react';
 
 export const NavBar = () =>{
     return(
-
+       <>
+        <nav>
+            <ul>
+                <li className = "index-link">
+                    <NavLink to ='/'>Index</NavLink>
+                </li>
+                <li className = "home-link">
+                    <NavLink to = '/home'>Home</NavLink>
+                </li>
+                <li className = "home-link">
+                    <NavLink to = '/notfound'>Not</NavLink>
+                </li>              
+               
+            </ul>
+        </nav>
     )
 }
